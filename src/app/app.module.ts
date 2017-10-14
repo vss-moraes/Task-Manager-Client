dfa3033087391c93e716cd320a1fafe5d3661c07
Vinicius Moraes <vsantana0212@gmail.com>
2017-10-14 16:19:34 -0400

    Criar estrutura básica com angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule, 
+         MatButtonModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +13,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
