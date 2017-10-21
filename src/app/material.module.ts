22574cdc07f72f804fb6868ecbc08a2444b40778
Vinicius Moraes <vsantana0212@gmail.com>
2017-10-21 08:32:33 -0300

    Melhorar detalhes da interface.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -11,7 +11,8 @@ import {
   MatNativeDateModule,
   MatSelectModule,
   MatSlideToggleModule,
-  MatExpansionModule
+  MatExpansionModule,
+  MatChipsModule
 } from '@angular/material';
 
 @NgModule({
@@ -26,7 +27,8 @@ import {
     MatNativeDateModule,
     MatSelectModule,
     MatSlideToggleModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatChipsModule
   ],
   exports: [
     MatButtonModule,
@@ -39,7 +41,8 @@ import {
     MatNativeDateModule,
     MatSelectModule,
     MatSlideToggleModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatChipsModule
   ]
 })
 export class MaterialModule {}
